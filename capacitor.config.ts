c7536411276f3c6c2017d8b8a6a842f349e45196
Syed Hunain Ali <syed.hunain@example.com>
2025-09-19 16:59:06 +0500

    10110 UPDATED : Dashboard.tsx Expanses.tsx database.ts Sales.tsx

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,32 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'com.salesmanager.app',
-	appName: 'Sales Manager',
-	webDir: 'dist',
-	server: {
-		androidScheme: 'https',
-		url: 'https://sales-pro-manager.vercel.app',
-		cleartext: false
-	}
+  appId: 'com.salesmanager.app',
+  appName: 'Sales Manager',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    url: 'https://sales-pro-manager.vercel.app',
+    cleartext: false
+  },
+  android: {
+    buildOptions: {
+      keystorePath: 'keystore.jks',
+      keystoreAlias: 'key0',
+    },
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
